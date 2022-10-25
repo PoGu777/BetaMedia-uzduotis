55a7573aaa83771ecc225a8e3830a284b13611ab
PoGu777 <81348579+PoGu777@users.noreply.github.com>
2022-10-25 23:19:33 +0300

    Add files via upload

---
+++ src/Components/FirstOffer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './FirstOffer.css';
+
+function FirstOffer {
+    return (
+        <div className='container'>
+            <div className='container_header'>
+
+            </div>
+            <div className='container_body'>
+                <div className='offer_heading'>
+
+                </div>
+                <div className='offer_location'>
+
+                </div>
+                <div className='offer_details'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className='header_text'>
+          <h3 className='text_heading'>Poilsis</h3>
+          <p className='text'>Must you with him from him her were more. In eldest be it result should remark vanity square. Unpleasant especially assistance sufficient he comparison so inquietude. Branch one shy edward stairs turned has law wonder horses. Devonshire invitation discovered out indulgence the excellence preference. Objection estimable discourse procuring he he remaining on distrusts. Simplicity affronting inquietude for now sympathize age. She meant new their sex could defer child. An lose at quit to life do dull.</p>
+        </div>
+    </div>
+  );
+}
+
+export default Header;
